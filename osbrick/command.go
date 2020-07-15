471379b1ef26a210be04a0f4fd515090accd46f9
whywaita <whywaita@whywrite.it>
2020-07-15 15:47:09 +0900

    catch *exec.Error

--- osbrick/command.go
+++ osbrick/command.go
@@ -18,9 +18,13 @@ func iscsiadmBase(ctx context.Context, args []string) ([]byte, int, error) {
 	logf("execute iscsiadm command [args: %s]", args)
 	out, err := exec.CommandContext(ctx, BinaryIscsiadm, args...).CombinedOutput()
 	if err != nil {
-		e := err.(*exec.ExitError) // exec.Run() return ExitError and normal error, but this code not catch normal error.
-		exitCode := e.Sys().(syscall.WaitStatus).ExitStatus()
-		return nil, exitCode, fmt.Errorf("failed to execute command (args: %v): %w", args, err)
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			// it's ExitError
+			exitCode := exitErr.Sys().(syscall.WaitStatus).ExitStatus()
+			return nil, exitCode, fmt.Errorf("failed to execute command (args: %v): %w", args, err)
+		}
+
+		return nil, 1, fmt.Errorf("failed to execute command (args: %v): %w", args, err)
 	}
 
 	return out, 0, nil
