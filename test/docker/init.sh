3c9cce720fddbd16d0b1eef9f4d95ecd6732af3b
whywaita <whywaita@whywrite.it>
2020-10-10 19:08:01 +0900

    disable CHAP authentication in tpg1

--- test/docker/init.sh
+++ test/docker/init.sh
@@ -13,6 +13,7 @@ targetcli /backstores/fileio create disk01 /iscsi_data/disk01 1G
 targetcli /iscsi create iqn.0000-00.com.example:target0
 targetcli /iscsi/iqn.0000-00.com.example:target0/tpg1/luns create /backstores/fileio/disk01
 targetcli /iscsi/iqn.0000-00.com.example:target0/tpg1/acls create iqn.0000-00.com.example:initiator0
+targetcli /iscsi/iqn.0000-00.com.example:target0/tpg1 set attribute authentication=0
 
 targetcli saveconfig
 
